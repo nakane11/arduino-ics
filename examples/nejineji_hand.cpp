440b06c9f97242c8bbcd14ef2a12023ba9783f23
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2025-08-07 21:56:41 +0900

    read servo id and display

---
+++ examples/nejineji_hand.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <IcsHardSerialClass.h>
+#include <RunningStats.h>
+#include <M5AtomS3.h>
+#include <button_manager.h>
+
+#include <vector>
+
+std::vector<RunningStats> stats_vector;
+
+#define DEBUG_SERIAL M5.Lcd
+
+#ifndef EN_PIN
+    #define EN_PIN -1
+#endif
+
+#ifndef RX_PIN
+    #define RX_PIN 18
+#endif
+
+#ifndef TX_PIN
+    #define TX_PIN 17
+#endif
+
+const int TIMEOUT = 10;
+IcsHardSerialClass *krs;
+std::vector<int> ids;
+int position;
+char log_msg[50];
+int cnt = 0;
+ButtonManager button_manager;
+
+void setup() {
+    DEBUG_SERIAL.begin();
+    DEBUG_SERIAL.println("start");
+    // Wait for board to stabilize
+    delay(2000);
+
+    const long BAUDRATE = 1250000;
+    Serial1.begin(BAUDRATE, SERIAL_8E1, RX_PIN, TX_PIN, false, TIMEOUT);
+    krs = new IcsHardSerialClass(&Serial1, BAUDRATE, TIMEOUT, EN_PIN);
+    krs->begin();
+    while (ids.size() < 19) {
+        ids.clear();
+        stats_vector.clear();
+
+        uint32_t idBits = krs->scanIDs();
+        for (int servo_id = 0; servo_id < 32; ++servo_id) {
+            if (static_cast<int>(idBits) & (1 << servo_id)) {
+                ids.push_back(servo_id);
+                RunningStats stats;
+                stats_vector.push_back(stats);
+            }
+        }
+        for (size_t i = 0; i < ids.size(); ++i) {
+          DEBUG_SERIAL.printf("%d,", ids[i]);
+        }
+        DEBUG_SERIAL.print("\n");
+    }
+    button_manager.createTask(0);
+}
+
+void loop() {
+  DEBUG_SERIAL.clear();
+  DEBUG_SERIAL.println(button_manager.getButtonState());
+  delay(500);
+}
